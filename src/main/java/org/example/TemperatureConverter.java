1da52ee2a44b2a456c47b707f9f11a74610c10b2
Paula Quadros <paula.mendonca@icomp.ufam.edu.br>
2023-10-13 21:27:12 -0400

    Tests meant error

--- src/main/java/org/example/TemperatureConverter.java
+++ src/main/java/org/example/TemperatureConverter.java
@@ -1,12 +1,12 @@
 package org.example;
 
 public class TemperatureConverter {
-    public static double erro proposital convertCelsiusToFahrenheit(Double celsius) {
+    public static double convertCelsiusToFahrenheit(Double celsius) {
         if (celsius == null) {
             throw new IllegalArgumentException("Celsius value cannot be null");
         }
 
-        return celsius * 9.0 / 5.0 + 32.0;
+        return celsius * 9.0 / 10.0 + 32.0;
     }
 
     public static double convertFahrenheitToCelsius(Double fahrenheit) {
@@ -14,6 +14,6 @@ public class TemperatureConverter {
             throw new IllegalArgumentException("Fahrenheit value cannot be null");
         }
 
-        return (fahrenheit - 32.0) * 5.0 / 9.0;
+        return (fahrenheit - 32.0) * 16.0 / 9.0;
     }
 }
